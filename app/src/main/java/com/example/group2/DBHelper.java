c762885de41c37cbc511ccbf6f62ef63c63f255e
dvhwang <christieng1997@hotmail.com>
2019-04-23 04:31:15 +1000

    DB finalised

--- app/src/main/java/com/example/group2/DBHelper.java
+++ app/src/main/java/com/example/group2/DBHelper.java
@@ -1,10 +1,15 @@
 package com.example.group2;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.group2.QuizContainer.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 
@@ -15,7 +20,7 @@ public class DBHelper extends SQLiteOpenHelper {
     //Reference to the database itself
     private SQLiteDatabase db;
 
-    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -39,8 +44,8 @@ public class DBHelper extends SQLiteOpenHelper {
         //To execute the SQL query above
         db.execSQL(SQL_CREATE_QUESTION_TABLE);
 
-        //Metho to insert the questions inside the table
-        insertQuestions();
+        //Invoke method which will have all the question
+        makeQuestions();
     }
 
     //Updates the table, if there are changes made to the existing table
@@ -50,8 +55,55 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    //Method to insert the questions
-    private void insertQuestions(){
-        Question q1 = new Question()
+
+    private void makeQuestions(){
+        Question q1 = new Question();
+        insertQuestion(q1);
+    }
+
+    //Adds the questions into database
+    private void insertQuestion(Question question){
+        ContentValues content = new ContentValues();
+
+        //To set which values belongs to which column
+        content.put(QuestionTable.COLUMN_QUESTION, question.getQuestion());
+        content.put(QuestionTable.COLUMN_OPTION1, question.getOption1());
+        content.put(QuestionTable.COLUMN_OPTION2, question.getOption2());
+        content.put(QuestionTable.COLUMN_OPTION3, question.getOption3());
+        content.put(QuestionTable.COLUMN_OPTION4, question.getOption4());
+        content.put(QuestionTable.COLUMN_ANSWER_NO, question.getAnswerNo());
+
+        //Insert the corresponding values into the table
+        db.insert(QuestionTable.TABLE_NAME, null, content);
+    }
+
+    //To retrieve the all the questions
+    public List<Question> getAllQuestions(){
+        List<Question> questionList = new ArrayList<>();
+
+        //Open the database for reading
+        db = getReadableDatabase();
+
+        //Querying the database to return all the values inside our Question table
+        Cursor cursor = db.rawQuery("SELECT * FROM " + QuestionTable.TABLE_NAME, null);
+
+        //Perform action if there is an entry on the first line
+        if (cursor.moveToFirst()){
+            do {
+                //Setting the values
+                Question question = new Question();
+                question.setQuestion(cursor.getString(cursor.getColumnIndex(QuestionTable.COLUMN_QUESTION)));
+                question.setOption1(cursor.getString(cursor.getColumnIndex(QuestionTable.COLUMN_OPTION1)));
+                question.setOption2(cursor.getString(cursor.getColumnIndex(QuestionTable.COLUMN_OPTION2)));
+                question.setOption3(cursor.getString(cursor.getColumnIndex(QuestionTable.COLUMN_OPTION3)));
+                question.setOption4(cursor.getString(cursor.getColumnIndex(QuestionTable.COLUMN_OPTION4)));
+                question.setAnswerNo(cursor.getInt(cursor.getColumnIndex(QuestionTable.COLUMN_ANSWER_NO)));
+
+                //Adding these values to the Question list
+                questionList.add(question);
+            } while(cursor.moveToNext()); //move to the next entry if there is an entry that exists on the first line
+        }
+        cursor.close();
+        return questionList;
     }
 }
--- app/src/main/java/com/example/group2/QuizMain.java
+++ app/src/main/java/com/example/group2/QuizMain.java
@@ -1,13 +1,165 @@
 package com.example.group2;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
 
 public class QuizMain extends AppCompatActivity {
+    //Initialising the widgets
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewQuestion;
+    private RadioGroup radioGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button submitBtn;
+
+    private ColorStateList textColorDefaultRb;
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_main);
+
+        //Assigning the widgets to its corresponding XML widget
+        textViewScore = findViewById(R.id.scoreText);
+        textViewQuestionCount = findViewById(R.id.countText);
+        textViewQuestion = findViewById(R.id.questionText);
+        radioGroup = findViewById(R.id.radioGroup);
+        rb1 = findViewById(R.id.radioButton1);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
+        rb4 = findViewById(R.id.radioButton4);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        textColorDefaultRb = rb1.getTextColors();
+
+        DBHelper dbHelper = new DBHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+
+        //Getting questions in a random order
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               if (!answered){ //if the question was not answered
+                   if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()){
+                       checkAnswer(); //if any of the radio buttons are selected, check the answer
+                   } else {
+                       Toast.makeText(QuizMain.this, "Please Select an Answer", Toast.LENGTH_SHORT).show();
+                   }
+               } else {
+                   showNextQuestion();
+               }
+           }
+        });
+    }
+
+    private void showNextQuestion(){
+        //Setting the radio buttons to a default colour before it will be changed
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        rb4.setTextColor(textColorDefaultRb);
+        radioGroup.clearCheck(); //to unselect the radio buttons for each new question
+
+        //Testing to verify if there are any more questions left
+        if (questionCounter < questionCountTotal){
+            currentQuestion = questionList.get(questionCounter);
+
+            //Setting the XML text with our question bank
+            textViewQuestion.setText(currentQuestion.getQuestion());
+
+            //Setting the different answers for the options
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+            rb4.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            //Setting the values for the current question and the total questions
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false; //locking the question
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void checkAnswer(){
+        answered = true;
+
+        RadioButton radioButtonSelected = findViewById(radioGroup.getCheckedRadioButtonId());
+        int answerNo = radioGroup.indexOfChild(radioButtonSelected) + 1;
+
+        //If the answer selected is correct
+        if (answerNo == currentQuestion.getAnswerNo()){
+            score++; //increase score
+            textViewScore.setText("Score: " + score); //display the increased score
+        }
+
+        showSolution();
+    }
+
+    private void showSolution(){
+        //Set the default colour of the radio button to red
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+        rb4.setTextColor(Color.RED);
+
+        //Change the correct answer's radio button to green
+        switch(currentQuestion.getAnswerNo()){
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 1 is Correct!");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 2 is Correct!");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 3 is Correct!");
+                break;
+            case 4:
+                rb4.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 4 is Correct!");
+                break;
+        }
+
+        if(questionCounter < questionCountTotal){
+            submitBtn.setText("Next Question");
+        } else {
+            //if it is the last question, change the button to signify the quiz has finished
+            submitBtn.setText("Finish Quiz");
+        }
+    }
+
+    private void finishQuiz(){
+        finish();
     }
 }
