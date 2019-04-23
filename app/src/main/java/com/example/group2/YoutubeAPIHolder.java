b2c75cafb65938d83f4cb39f0815d72b5f9769a5
dvhwang <christieng1997@hotmail.com>
2019-04-23 15:00:45 +1000

    Youtube API Key Stored

---
+++ app/src/main/java/com/example/group2/YoutubeAPIHolder.java
@@ -0,0 +1,15 @@
+package com.example.group2;
+
+//Class to hold the YoutubeAPIKey
+public class YoutubeAPIHolder {
+
+    public YoutubeAPIHolder() { }
+
+    //Storing the API Key
+    private static final String API_KEY = "AIzaSyBT-38WzeSc6623ef6VlSD4fSMyvPUN6eM";
+
+    //Returning the API Key
+    public static String getApiKey(){
+        return API_KEY;
+    }
+}
