1dff4fdb09a2078642ee355b413996a6a7a8549f
dvhwang <davohwang@gmail.com>
2019-04-27 19:27:31 +1000

    More Comments

--- app/src/main/java/com/example/group2/DetailActivity.java
+++ app/src/main/java/com/example/group2/DetailActivity.java
@@ -21,6 +21,7 @@ public class DetailActivity extends AppCompatActivity {
             arguments.putString(DetailFragment.ARG_ITEM_ID, getIntent().getStringExtra(DetailFragment.ARG_ITEM_ID));
             DetailFragment fragment = new DetailFragment();
             fragment.setArguments(arguments);
+
             //Inserting the fragment XML file to the placeholder
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.detail_container, fragment)
