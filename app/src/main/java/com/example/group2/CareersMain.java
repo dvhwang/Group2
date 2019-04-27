21a8f2eb4f8806df8e4cb02566d4608a2c741e12
patwan112 <pwan05@gmail.com>
2019-04-28 07:37:28 +0800

    Commenting on CareersMain

--- app/src/main/java/com/example/group2/CareersMain.java
+++ app/src/main/java/com/example/group2/CareersMain.java
@@ -22,6 +22,8 @@ import okhttp3.Response;
 
 public class CareersMain extends AppCompatActivity {
 
+//Instantiate all variables and layout widgets
+
     private Button btn_mck;
     private Button btn_bain;
     private Button btn_bcg;
@@ -30,6 +32,7 @@ public class CareersMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		// Find all widgets and buttons using their unique ID from the OnCreate Method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_careers_main);
 
@@ -37,7 +40,9 @@ public class CareersMain extends AppCompatActivity {
         btn_bain = findViewById(R.id.btn_bain);
         btn_bcg = findViewById(R.id.btn_bcg);
         Wiki_Summary = findViewById(R.id.Wiki_Summary);
-
+		
+		
+//Methods which set the link pertaining to specific Wikipedia articles based off the button clicked
         btn_mck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,15 +66,18 @@ public class CareersMain extends AppCompatActivity {
 
     }
 
-    public void getWikiPage(String title){
-
+   //This program downloads the JSON Wikipedia file
+        // – title variable represents the unique section of each distinct url for specific pages
         OkHttpClient client = new OkHttpClient();
         String url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + title;
 
-
+        //Creates request object for making network calls
         Request request = new Request.Builder()
                 .url(url)
                 .build();
+
+        //Use the client to create a Call object
+        //Use enqueue method for asynchronous calls
 
 
         client.newCall(request).enqueue(new Callback() {
@@ -81,12 +89,15 @@ public class CareersMain extends AppCompatActivity {
             @Override
             public void onResponse(okhttp3.Call call, Response response) throws IOException {
                 final Response wikiResponse = response;
+                //onResponse method invoked when HTTP call to Wikipedia is successful
 
-                CareersMain.this.runOnUiThread(new Runnable() {
+                //In order to update the UI, the runOnUiThread() is used to sync with the UI thread
+                WikiSearchActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         String data = " ";
 
+                        //Use try catch block to handle exceptions when retrieving body of Wikipedia page as a string
 
                         try{
                             data = wikiResponse.body().string();
@@ -95,18 +106,23 @@ public class CareersMain extends AppCompatActivity {
                         }
                         try{
                             String pagenumber = "";
-
-                            Iterator<String> iterator = new JSONObject(data).getJSONObject("query").getJSONObject("pages").keys();
+                            //Iterator interface used to traverse collection of wiki pages
+                            Iterator <String> iterator = new JSONObject(data).getJSONObject("query").getJSONObject("pages").keys();
                             while (iterator.hasNext()){
                                 pagenumber = iterator.next();
                             }
+
+                            //Because the content summary is in the extract object of JSON file,
+                            // must use the get() method to retrieve all objects up to and
+                            // including the extract object so as to not return any extra
+                            // characters from JSON file
 
                             data = new JSONObject(data).getJSONObject("query").getJSONObject("pages").getJSONObject(pagenumber).getString("extract");
                         }catch (JSONException e){
                             e.printStackTrace();
                         }
-                        //Setting the TextView Here
-                        Wiki_Summary.setText(data);
+                        //Change the textview here
+                        text_wiki_sum.setText(data);
                     }
                 });
 
